bd00cbcf8aa8bdb30aab30b1663eb8161f00b763
Усевалад Мартысюк | Usevalad Martysiuk <wsmartysuk@gmail.com>
2025-01-25 19:07:00 +0100

    Messages appearing
    - 'Social app' greybox is finished.

---
+++ GGJ2025/Assets/Game files/Scripts/Inside Window Scripts/NewMessagesScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NewMessagesScript : MonoBehaviour
+{
+    [SerializeField] private GameObject itemPrefab;
+    [SerializeField] private Transform contentTransform; 
+    [SerializeField] private ScrollRect scrollRect;     
+
+    public void AddItem()
+    {
+        GameObject newItem = Instantiate(itemPrefab, contentTransform);
+        newItem.transform.SetSiblingIndex(0);
+        ScrollToBottom();
+    }
+
+    private void ScrollToBottom()
+    {
+        Canvas.ForceUpdateCanvases(); 
+        scrollRect.verticalNormalizedPosition = 0; 
+    }
+}
